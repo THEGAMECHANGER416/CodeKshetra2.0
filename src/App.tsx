0650373e99b3e30d69fa90788cb72b9a22699712
Aditya <adityakumarji74@gmail.com>
2024-10-12 11:06:30 +0530

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant={"outline"} className="bg-red-400">
+        Click me
+      </Button>
+    </>
+  );
+}
+
+export default App;
