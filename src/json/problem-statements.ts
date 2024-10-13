d567ddeff54b611e54b794ddaa2de57bec710157
sanchxt <tpbeast007@gmail.com>
2024-10-14 03:05:57 +0530

    add dummy data for problem statements

---
+++ src/json/problem-statements.ts
@@ -0,0 +1,40 @@
+import { ProblemStatement } from "@/types/problems";
+
+export const problemStatements: ProblemStatement[] = [
+  {
+    title: "Sanchit Problem 1",
+    category: "Some Random Category 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+  {
+    title: "Sanchit Problem 2",
+    category: "Some Random Category 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+  {
+    title: "Sanchit Problem 3",
+    category: "Some Random Category 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+  {
+    title: "Sanchit Problem 4",
+    category: "Some Random Category 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+  {
+    title: "Sanchit Problem 5",
+    category: "Some Random Category 5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+  {
+    title: "Sanchit Problem 6",
+    category: "Some Random Category 6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+  },
+];
