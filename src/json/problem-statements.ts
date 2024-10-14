5f58cd97ebc1d20025a5ac0c5572b68a843892ac
sanchxt <tpbeast007@gmail.com>
2024-10-14 14:22:40 +0530

    change problem statements data

--- src/json/problem-statements.ts
+++ src/json/problem-statements.ts
@@ -4,39 +4,39 @@ export const CHANGE_DURATION = 5000;
 
 export const problemStatements: ProblemStatement[] = [
   {
-    title: "Sanchit Problem 1",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 1",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
   {
-    title: "Sanchit Problem 2",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 2",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
   {
-    title: "Sanchit Problem 3",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 3",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
   {
-    title: "Sanchit Problem 4",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 4",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
   {
-    title: "Sanchit Problem 5",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 5",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
   {
-    title: "Sanchit Problem 6",
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     category: "Some Random Category 6",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit eveniet molestiae inventore. Ducimus aliquam natus exercitationem corrupti? Aut porro maiores voluptas rerum quidem! Voluptatibus dignissimos sapiente iste rem aperiam.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eos repellendus natus corrupti delectus voluptatibus, provident ducimus, dignissimos saepe fugit et iste est vero? Nobis quaerat labore autem corrupti consequatur, repellat, non optio officiis iusto consectetur eum. Odio dolores consequuntur vitae minima. Excepturi dolorum deserunt aperiam asperiores, repudiandae reiciendis obcaecati, vero quod necessitatibus laborum corporis explicabo culpa eligendi incidunt architecto.",
   },
 ];
