a3abc51645a8d916d2539881211f2ce4a9ffc35f
Aditya <adityakumarji74@gmail.com>
2024-10-17 20:31:11 +0530

    home page changed

--- src/components/custom/Home.tsx
+++ src/components/custom/Home.tsx
@@ -1,92 +1,58 @@
-import { Button } from "@/components/ui/button";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
-import { FaFacebook } from "react-icons/fa6";
-import Autoplay from "embla-carousel-autoplay";
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-
+import { Button } from "../ui/button";
 import data from "../../json/home.json";
-import React from "react";
 
 const Home = () => {
-  const plugin1 = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
-  );
-  const plugin2 = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
-  );
-
   return (
     <>
       <div>
-        <div className="flex h-full lg:h-screen w-full flex-col items-center justify-end rounded-lg">
-          <div className="grid h-full w-full grid-cols-8 grid-rows-10 gap-4">
-            <div className="col-span-4 sm:col-span-2 row-span-2 mt-20 sm:mt-0  rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-xl">
-                Registrations
+        <div className="flex h-full lg:h-[93vh] w-full flex-col items-center justify-end rounded-lg">
+          <div className="grid h-full w-full grid-cols-5 grid-rows-9 gap-4">
+            <div className="col-span-4 row-span-6 rounded-3xl  bg-[url('/assets/bg.png')] bg-cover bg-center flex flex-col pl-6 justify-end pb-12">
+              <div className="font-bold mt-4 text-white text-start text-6xl">
+                Codeक्षेत्र: The Ultimate
               </div>
-              <div className="font-bold mt-4 text-white text-xl">
-                {data.regsiterations}
-              </div>
-            </div>
-
-            <div className="col-span-full sm:col-span-4 row-span-3 sm:h-full sm:mb-0 row-start-2 sm:row-start-1 sm:col-start-3  sm:row-span-5 relative rounded-3xl bg-secondary">
-              <div className="font-bold mt-4 text-white text-4xl">
-                Codeक्षेत्र: The Ultimates
-              </div>
-              <div className="font-bold mt-2 sm:mt-4 text-black text-4xl">
+              <div className="font-bold mt-2 sm:mt-4 text-start text-white text-6xl">
                 36-Hour Hackathon
               </div>
-              <Button className="mt-4 text-lg h-12 p-3 w-40 rounded-xl">
+              <Button className="mt-4 text-2xl h-14 p-3 w-48 rounded-xl text-black font-bold  bg-pink">
                 Register
               </Button>
-              <img
-                src="/assets/logo.png"
-                alt="logo"
-                className="absolute bottom-0 left-1/2 bg-primary transform -translate-x-1/2 translate-y-1/2 h-44 w-44 sm:w-36 sm:h-36 md:w-40 md:h-40 lg:w-80 lg:h-80 rounded-full border-2 border-background "
-              />
             </div>
 
-            <div className="col-span-full sm:col-span-2 row-span-1 row-start-1 sm:col-start-7 rounded-3xl bg-[#D9D9D9]">
-              <div className="font-bold mt-4 text-xl">logos</div>
+            <div className="col-span-1 row-span-2">
+              <img src="/assets/logo.png" alt="" />
             </div>
-
-            <div className="col-span-4 sm:col-span-2 row-span-2 mt-20 sm:mt-0 rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-xl">
+            <div className="col-span-1 row-span-2 rounded-3xl bg-secondary">
+              <div className="font-bold text-pink mt-4 text-3xl">
                 Prize Pool
               </div>
-              <div className="font-bold mt-4 text-white text-xl">
+              <div className="font-bold mt-4 text-white text-2xl">
                 {data.prizePool}
               </div>
             </div>
-
-            <div className="col-span-2 row-span-3 rounded-3xl bg-primary hidden sm:block">
-              <div className="font-bold  text-white text-3xl mt-20">
-                {data.extraBox}
-              </div>
-            </div>
-
-            <div className="col-span-4 sm:col-span-2 row-span-2 rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-xl">Location</div>
-              <div className="font-bold mt-4 text-white text-xl">
+            <div className="col-span-1 row-span-2 rounded-3xl bg-secondary">
+              <div className="font-bold text-pink mt-4 text-3xl">Location</div>
+              <div className="font-bold mt-1 text-white text-2xl">
                 {data.location}
               </div>
             </div>
 
-            <div className=" col-span-full sm:col-span-3 row-span-2 sm:row-span-4 rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-2xl text-center lg:text-start lg:pl-16 ">
+            <div className="col-span-3 row-span-3 rounded-3xl bg-secondary">
+              <div className=" text-pink mt-4 pl-7 text-6xl text-start font-bebas">
+                Registrations
+              </div>
+              <div className="font-bold text-9xl text-white">
+                {data.regsiterations}+
+              </div>
+            </div>
+
+            <div className="col-span-2 row-span-3 rounded-3xl bg-secondary">
+              <div className="font-bold text-start pl-6 text-pink mt-4 text-3xl">
                 Event in
               </div>
-              <div className="flex space-x-4 items-center text-white justify-center mt-9">
-                <div className="text-center">
+
+              <div className="flex space-x-4 items-center text-white justify-center mt-5">
+                <div className="text-center bg-background p-4 rounded-xl">
                   <p className="lg:text-6xl md:text-4xl sm:text-2xl text-3xl font-bold ">
                     30
                   </p>
@@ -95,7 +61,7 @@ const Home = () => {
                 <div className="lg:text-6xl md:text-4xl sm:text-2xl text-3xl font-bold mb-6">
                   :
                 </div>
-                <div className="text-center">
+                <div className="text-center  bg-background p-4 rounded-xl">
                   <p className="lg:text-6xl md:text-4xl sm:text-2xl text-3xl font-bold">
                     30
                   </p>
@@ -104,104 +70,13 @@ const Home = () => {
                 <div className="lg:text-6xl md:text-4xl sm:text-2xl text-3xl font-bold mb-6">
                   :
                 </div>
-                <div className="text-center">
+                <div className="text-center  bg-background p-4 rounded-xl">
                   <p className="lg:text-6xl md:text-4xl sm:text-2xl text-3xl font-bold">
                     30
                   </p>
                   <p className="text-sm sm:text-xs">Seconds</p>
                 </div>
               </div>
-              <div className="font-bold text-white mt-5 pb-5 text-2xl">
-                What are you waiting for ?
-              </div>
-            </div>
-
-            <div className="col-span-4 row-start-7 col-start-5 sm:col-span-2 row-span-2 sm:row-start-8 sm:col-start-4 rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-xl">
-                Our Socials
-              </div>
-
-              <div className="sm:flex hidden justify-evenly flex-wrap text-3xl mt-4 ">
-                <FaInstagram className="text-secondary" />
-                <FaXTwitter className="text-secondary" />
-                <MdEmail className="text-secondary" />
-                <FaFacebook className="text-secondary" />
-              </div>
-
-              <div className="sm:hidden flex flex-col pt-6 space-y-5">
-                <div className="flex justify-evenly">
-                  <FaInstagram className="text-secondary text-4xl" />
-                  <FaXTwitter className="text-secondary text-4xl" />
-                </div>
-
-                <div className="flex justify-evenly">
-                  <MdEmail className="text-secondary text-4xl" />
-                  <FaFacebook className="text-secondary text-4xl" />
-                </div>
-              </div>
-            </div>
-
-            <div className="col-span-full sm:col-span-3 row-span-4 hidden sm:block rounded-3xl bg-primary">
-              <div className="font-bold text-accent mt-4 text-xl">
-                Event Highlights
-              </div>
-              <div className="w-full h-5/6 flex justify-center items-center">
-                <Carousel
-                  plugins={[plugin1.current]}
-                  className="bg-transparent w-full h-full"
-                >
-                  <CarouselContent>
-                    {data.eventHighlights.map((src, index) => (
-                      <CarouselItem key={index} className="bg-transparent">
-                        <Card className="bg-transparent w-full h-full">
-                          <CardContent className="flex items-center bg-transparent justify-center w-full h-full">
-                            <div className="bg-transparent text-white h-full w-full p-2">
-                              <img
-                                src={src}
-                                alt=""
-                                className="object-contain h-full w-full"
-                              />
-                            </div>
-                          </CardContent>
-                        </Card>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                  <CarouselPrevious />
-                  <CarouselNext />
-                </Carousel>
-              </div>
-            </div>
-          </div>
-          <div className=" w-full mt-4 h-64 block sm:hidden rounded-3xl bg-primary">
-            <div className="font-bold text-accent mt-4 text-xl">
-              Event Highlights
-            </div>
-            <div className="w-full h-5/6 flex justify-center items-center">
-              <Carousel
-                plugins={[plugin2.current]}
-                className="bg-transparent w-full h-full"
-              >
-                <CarouselContent>
-                  {data.eventHighlights.map((src, index) => (
-                    <CarouselItem key={index} className="bg-transparent">
-                      <Card className="bg-transparent w-full h-full">
-                        <CardContent className="flex items-center bg-transparent justify-center w-full h-full">
-                          <div className="bg-transparent text-white h-full w-full p-2">
-                            <img
-                              src={src}
-                              alt=""
-                              className="object-contain h-full w-full"
-                            />
-                          </div>
-                        </CardContent>
-                      </Card>
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-              </Carousel>
             </div>
           </div>
         </div>
