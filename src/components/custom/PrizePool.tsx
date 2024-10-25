6cbad740139c2e6f35a8b49025a5b702a4505e89
Naman Dadhich <namandadhich15592@gmail.com>
2024-10-26 00:51:22 +0530

    Sponsor

--- src/components/custom/PrizePool.tsx
+++ src/components/custom/PrizePool.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-const PrizePool: React.FC = () => {
-  return (
-    <div className="bg-green-100 p-8 rounded-lg shadow-lg mx-auto max-w-2xl">
-      <h1 className="text-3xl text-green-800 font-semibold mb-4">Prizepool</h1>
-      <p className="text-green-700 text-lg leading-relaxed">
-        Here we outline the key problems we aim to solve through our efforts...
-      </p>
-    </div>
-  );
+const Prizes: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-white p-4">
+            <h1 className="md:text-[12rem] font-bebas text-6xl text-pink font-bold mb-[4rem] md:mb-[6rem] text-center">
+                PRIZES
+            </h1>
+        </div>
+    );
 };
 
-export default PrizePool;
+export default Prizes;
